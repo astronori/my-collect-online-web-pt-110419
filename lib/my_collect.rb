ce9fb6048fa0010cfc32397cd6cb05648834ca0f
kenneth gil <astronori@gmail.com>
2019-11-30 17:45:26 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,6 +1,8 @@
 nicknames = ["Kenny G", "Astronori", "K-Tut"]
 def my_collect(array)
   i = 0 
+  name_collection = []
+while i < array.length
   name_collection.push yield (array[i])
   i += 1
 end
